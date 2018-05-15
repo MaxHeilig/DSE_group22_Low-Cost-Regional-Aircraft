37f0691925c4879ae057cfe10c59d1aa24c7e150
AdrianLCK <adrian95@gmail.com>
2018-05-15 10:22:15 +0200

    Added limits
    Max positive and negative limit loads

--- FlightEnvelope.m
+++ FlightEnvelope.m
@@ -1,7 +1,27 @@
 clc % clear command line
 clear % clear workspace to reduce RAM pressure
 close % close figures
+% info for building flight envelope are obtained from 'V-n diagram" from
+% SEAD
 
-OEW = 1000; % kg
-CL_Max = 1.6; 
+%% conversions
+kg_to_lbs = 2.20462;
 
+%% inputs
+W_TO = 2200 * kg_to_lbs; % [lbs]
+CL_Max = 1.218; % [-]
+
+
+
+%% Maximum positive limit load factor, n_max (CS-25)
+
+if W_TO > 50000
+    n_max = 2.5;
+elseif W_TO > 4100
+    n_max = 2.1 + (24000/(W_TO + 10000));
+else
+    n_max = 3.8;
+end
+
+% Maximum negative limit load factor
+n_max_negative = -1 * n_max; % according to CS-25
